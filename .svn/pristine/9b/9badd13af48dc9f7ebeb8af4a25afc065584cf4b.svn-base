cb399388019b31fba827f55e10bc3072579554ae
2009ejsvk <2009ejsvk@gmail.com>
2026-01-07 06:50:54 +0900

    Fix: Apply .gitignore and remove tracked temporary files

---
+++ .svn/pristine/9b/9badd13af48dc9f7ebeb8af4a25afc065584cf4b.svn-base
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "ConstantBufferData.h"
+
+class CCBufferTransform :
+    public CConstantBufferData
+{
+public:
+	CCBufferTransform();
+	CCBufferTransform(const CCBufferTransform& ref);
+	CCBufferTransform(CCBufferTransform&& ref)	noexcept;
+	virtual ~CCBufferTransform();
+
+private:
+	FCBufferTransformData	mData;
+
+public:
+	virtual bool Init();
+	virtual void UpdateBuffer();
+	virtual CCBufferTransform* Clone();
+
+public:
+	void SetWorldMatrix(const FMatrix& World)
+	{
+		mData.World = World;
+	}
+
+	void SetViewMatrix(const FMatrix& View)
+	{
+		mData.View = View;
+	}
+
+	void SetProjMatrix(const FMatrix& Proj)
+	{
+		mData.Proj = Proj;
+	}
+};
+
--- Engine/Include/Component/Collider.h
+++ Engine/Include/Component/Collider.h
@@ -25,10 +25,18 @@ protected:
 	bool			mCollision = false;
 	FCollisionProfile* mProfile = nullptr;
 
+	// 이 충돌체와 부딪친 충돌체는 서로 떨어지기 전까지
+	// 이 맵에 보관해둔다.
+	std::unordered_map<CCollider*, std::weak_ptr<CCollider>>	mCollisionObjectMap;
+
 	std::weak_ptr<class CShader>	mShader;
 	std::weak_ptr<class CMesh>		mMesh;
 	std::shared_ptr<class CCBufferTransform>	mTransformCBuffer;
 	std::shared_ptr<class CCBufferCollider>	mColliderCBuffer;
+
+	std::function<void(const FVector3&, CCollider*)>	
+		mCollisionBeginFunc;
+	std::function<void(CCollider*)>	mCollisionEndFunc;
 
 public:
 	EColliderType GetColliderType()	const
@@ -46,6 +54,9 @@ public:
 		return mProfile;
 	}
 
+	bool CheckCollisionObject(CCollider* Collider);
+	void EraseCollisionObject(CCollider* Collider);
+
 public:
 	virtual void SetDebugDraw(bool DebugDraw);
 	void SetCollisionProfile(const std::string& Name);
@@ -60,7 +71,29 @@ protected:
 	virtual CCollider* Clone()	const = 0;
 
 public:
+	void CallCollisionBegin(const FVector3& HitPoint,
+		const std::weak_ptr<CCollider>& Dest);
+	void CallCollisionEnd(CCollider* Dest);
+
+public:
 	virtual bool Collision(FVector3& HitPoint,
 		std::shared_ptr<CCollider> Dest) = 0;
+
+public:
+	template <typename T>
+	void SetCollisionBeginFunction(T* Obj,
+		void(T::* Func)(const FVector3&, CCollider*))
+	{
+		mCollisionBeginFunc = std::bind(Func, Obj,
+			std::placeholders::_1, std::placeholders::_2);
+	}
+
+	template <typename T>
+	void SetCollisionEndFunction(T* Obj,
+		void(T::* Func)(CCollider*))
+	{
+		mCollisionEndFunc = std::bind(Func, Obj,
+			std::placeholders::_1);
+	}
 };
 
